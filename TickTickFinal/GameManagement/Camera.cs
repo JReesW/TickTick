4b93df6efbc8ed583ebac189c867ed79b923a597
JReesW <j.rees.w@gmail.com>
2018-10-31 15:35:50 +0100

    Added Camera class
    Created a new class called Camera etc.

---
+++ TickTickFinal/GameManagement/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTick5.GameManagement
+{
+    class Camera
+    {
+        public Camera()
+        {
+
+        }
+    }
+}
